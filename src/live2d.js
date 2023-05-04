79655934b8a0521aec3e677b2471395460e88b2a
Motohiro-Otsuka <omegarinp@gmail.com>
2023-05-04 12:57:14 +0900

    画面サイズに合わせて自動的にリサイズするように変更

--- src/live2d.js
+++ src/live2d.js
@@ -15,8 +15,8 @@ let {
 // 1, Live2Dモデルへのパスを指定する
 let modelUrl = "miku2023/miku2023.model3.json";
 let currentModel;
-let width = Math.floor(window.innerWidth * 0.9);
-let height = Math.floor(window.innerHeight * 0.9);
+let width = Math.floor(window.innerWidth);
+let height = Math.floor(window.innerHeight);
 
 // メインの処理開始
 async function loadmodule() {
@@ -87,4 +87,25 @@ function main(){
   loadmodule();
 }
 
+//画面サイズを自動的にリサイズ
+function screenResize() {
+  let wid = window.innerWidth;//ゲームを表示できる最大横幅
+  let hei = window.innerHeight;//ゲームを表示できる最大縦幅
+  let x =  window.innerWidth;
+  let y =  window.innerHeight;
+  app.stage.scale.x = app.stage.scale.y = 1;//スクリーン幅が十分の時は画面倍率を1にする
+  resizeRatio = Math.min(wid/width, hei/height);//横幅と縦幅の、ゲーム画面に対する比のうち小さい方に合わせる
+  
+  if(wid < width || hei < height) {//スクリーン幅が足りないとき
+      //リサイズ倍率を調整
+      x = width*resizeRatio; 
+      y = height*resizeRatio; 
+      app.stage.scale.x = resizeRatio;
+      app.stage.scale.y = resizeRatio;
+  }
+  
+  app.renderer.resize(x, y);//レンダラーをリサイズ
+}
+//画面サイズがリサイズされると発火する関数の定義
+window.addEventListener('resize',screenResize,false);
 main()
