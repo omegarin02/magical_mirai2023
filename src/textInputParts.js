ff65656a4a73e00656ef8b247028db0d69a69d9a
Motohiro-Otsuka <omegarinp@gmail.com>
2023-05-09 20:01:15 +0900

    テキストボックスPartsのコミット漏れ

---
+++ src/textInputParts.js
@@ -0,0 +1,3 @@
+const inputChatBoxHtml = '<div class="cyber-input ac-cyan gb-black fg-cyan" id="inputChatBox"> \
+    <input type="text" placeholder="チャットを入力してね" style="width:100%" /> \
+    </div>'
